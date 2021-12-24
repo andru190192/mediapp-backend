d5ea44e8196625fcf5c586bedf95fbbcc9ff567d
Carlos Guachisaca <caguachisaca@gmail.com>
2021-12-23 21:46:50 -0500

    initial commit

---
+++ src/main/java/com/andresguachisaca/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.andresguachisaca;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MediappBackendApplication.class);
+	}
+
+}
